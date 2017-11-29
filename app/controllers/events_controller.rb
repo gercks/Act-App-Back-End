81268b0d41adbbbe5b79369f4d92e64ab08d7a04
gercks <elizagercke@gmail.com>
2017-11-29 15:37:14 -0500

    Add successful curl scripts!
    They work in console
    Also change to inherit from application controller instead of protected
    for easier testing, but need to switch back later!

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,5 @@
 
-class ProtectedController < ApplicationController
-  before_action :authenticate
-end
-
-
-class EventsController < ProtectedController
+class EventsController < ApplicationController
   before_action :set_event, only: [:show, :update, :destroy]
 
   # GET /events
--- scripts/events/show-event.sh
+++ scripts/events/show-event.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 API="${API_ORIGIN:-http://localhost:4741}"
-URL_PATH="/events"
+URL_PATH="/events/"
 curl "${API}${URL_PATH}/$ID" \
   --include \
   --request GET \
