20184db21292d77fe8702a38f56c47505b6ccc11
gercks <elizagercke@gmail.com>
2017-11-30 15:58:53 -0500

    /cx jkl v

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129161451) do
+ActiveRecord::Schema.define(version: 20171130141436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20171129161451) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "examples", force: :cascade do |t|
@@ -41,5 +43,6 @@ ActiveRecord::Schema.define(version: 20171129161451) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "events", "users"
   add_foreign_key "examples", "users"
 end
--- scripts/auth/sign-in.sh
+++ scripts/auth/sign-in.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# TOKEN=BAhJIiUzZTAzM2UyMDQwZDYwOGRhNzlmYTg3MmNmZjkxYjdjMQY6BkVG--cb18d2d0a3d6a86cb7350bcf274348fa8bf6ee7b NAME='March for Equality' DATE='2017-12-16' DESCRIPTION="let's fix the world" sh scripts/events/create-event.sh
+# TOKEN=BAhJIiUzZTAzM2UyMDQwZDYwOGRhNzlmYTg3MmNmZjkxYjdjMQY6BkVG--cb18d2d0a3d6a86cb7350bcf274348fa8bf6ee7b NAME='March for Equality' DATE='2017-12-16' DESCRIPTION="let's fix everything' sh scripts/events/create-event.sh
 
 API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/sign-in"
